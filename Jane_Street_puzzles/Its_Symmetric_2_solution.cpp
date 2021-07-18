1923fcf5278dcd615238db3ea0225495269c24e2
thomasschafer <thomasschafer97@gmail.com>
2021-07-18 22:21:32 +0100

    adding function definitions that need to be filled in

--- Jane_Street_puzzles/Its_Symmetric_2_solution.cpp
+++ Jane_Street_puzzles/Its_Symmetric_2_solution.cpp
@@ -1,6 +1,8 @@
 #include <cmath>
+#include <fstream>
 #include <iostream>
 #include <map>
+#include <set>
 #include <vector>
 
 using std::vector;
@@ -60,6 +62,76 @@ vector<vector<int>> padded_factors_of_fixed_length(int n, int length, vector<int
         }
     }
     return output;
+}
+
+
+vector<vector<int>> permutations(vector<int>) {
+    vector<vector<int>> res;
+    return res;
+}
+
+bool shade(
+    vector<vector<int>> arr,
+    vector<vector<int>>  helper_arr,
+    int cur_row,
+    int cur_col,
+    vector<int> shade_combination
+) {
+    return true;
+}
+
+
+vector<vector<int>> rotate_by_90_cw(vector<vector<int>> arr) {
+    return arr;
+}
+
+bool is_symmetric(vector<vector<int>> arr) {
+    return true;
+}
+
+
+void clear_connected_component(vector<vector<int>> arr, int i, int j) {
+
+}
+
+bool is_connected(vector<vector<int>> arr) {
+    return true;
+}
+
+
+int area_of_connected_unshaded_area(vector<vector<int>> arr, int i, int j) {
+    return 0;
+}
+
+
+int sum_of_squares_of_connected_unshaded_areas(vector<vector<int>> arr) {
+    return 0;
+}
+
+
+int* next_position(int cur_row, int cur_col, int end_col) {
+    int new_position[2];
+    return new_position;
+}
+
+
+void solve_rec(
+    vector<vector<int>> arr,
+    vector<vector<int>> helper_arr,
+    int cur_row,
+    int cur_col,
+    int end_row,
+    int end_col,
+    std::set<std::string> array_states_checked,
+    std::fstream solutions_file,
+    int recursion_depth
+) {
+
+}
+
+
+void solve(vector<vector<int>> arr) {
+
 }
 
 
